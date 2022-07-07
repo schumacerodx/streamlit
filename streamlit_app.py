65a426dff9e28997a905f78d470271318c873c3d
schumacerodx <108867499+schumacerodx@users.noreply.github.com>
2022-07-07 05:34:10 -0400

    Update streamlit_app.py
    retrieve and show request headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
+from streamlit.script_run_context import get_script_run_ctx
+from streamlit.server.server import Server
 import streamlit as st
 
-st.title("Hello Dashboards!")
+session_id = get_script_run_ctx().session_id
+session_info = Server.get_current()._get_session_info(session_id)
+headers = session_info.ws.request.headers
+
+headers = headers._dict
+
+st.title("Hello Headers!")
 st.balloons()
+st.write(str(headers))
